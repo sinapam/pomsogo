b9476fa664e2ded3be3953796c94d0820bf1a601
juacompe <juacompe@gmail.com>
2012-06-14 17:58:01 +0700

    - test model with database

--- test/models/TestDeal.java
+++ test/models/TestDeal.java
@@ -2,19 +2,30 @@ package models;
 
 import static org.junit.Assert.*;
 
-import models.Deal;
+import models.*;
 
-import org.junit.Test;
+import org.junit.*;
+import play.test.*;
+
+import static play.test.Helpers.*;
 
 public class TestDeal {
 
 	@Test
 	public void test_create_new_deal() {
-		String title = "Korean Tour";
-		double price = 13500;
-		Deal deal = new Deal(title, price);
-		assertEquals(title, deal.title);
-		assertEquals(price, deal.price, 0);
+		running(fakeApplication(), new Runnable() {
+			
+			@Override
+			public void run() {
+				String title = "Korean Tour";
+				double price = 13500;
+				Deal deal = new Deal(title, price);
+				deal.save();
+				deal = Deal.find.byId(deal.id);
+				assertEquals(title, deal.title);
+				assertEquals(price, deal.price, 0);
+			}
+		});
 	}
 
 }
