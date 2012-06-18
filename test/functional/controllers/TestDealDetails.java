c993eb4d3342d87b78684c5bb5b74dac86794228
juacompe <juacompe@gmail.com>
2012-06-18 12:57:12 +0700

    add more deals in initial data

--- test/functional/controllers/TestDealDetails.java
+++ test/functional/controllers/TestDealDetails.java
@@ -8,12 +8,22 @@ import play.test.FunctionalTest;
 public class TestDealDetails extends FunctionalTest {
 
 	@Test
-	public void testGetDealDetails() {
-        Response response = GET("/application/deal_details?id=1");
+	public void testGetKoreanTourDealDetails() {
+        Response response = GET("/deal/1");
         assertIsOk(response);
         assertContentType("text/html", response);
-        assertContentMatch("Korean Tour", response);
+        assertContentMatch("ทัวร์เกาหลี", response);
         assertContentMatch("13500", response);
+
+	}
+
+	@Test
+	public void testGetAuroraSpaDealDetails() {
+        Response response = GET("/deal/2");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        assertContentMatch("ออโรร่าสปา", response);
+        assertContentMatch("2500", response);
 
 	}
 
