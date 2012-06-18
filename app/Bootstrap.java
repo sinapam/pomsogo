d648d7ea0dbee4bac486792d6ef66261d29f0636
juacompe <juacompe@gmail.com>
2012-06-18 13:42:52 +0700

    - fix the problem of duplicate id from ids caching when load initial data

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,17 +1,18 @@
 import play.*;
 import play.jobs.*;
 import play.test.*;
- 
+
 import models.*;
- 
+
 @OnApplicationStart
 public class Bootstrap extends Job {
- 
-    public void doJob() {
-        // Check if the database is empty
-        if(Deal.count() == 0) {
-            Fixtures.loadModels("initial-data.yml");
-        }
-    }
- 
+
+	public void doJob() {
+		// Check if the database is empty
+		if (Deal.count() == 0) {
+			Fixtures.delete();
+			Fixtures.loadModels("initial-data.yml");
+		}
+	}
+
 }
